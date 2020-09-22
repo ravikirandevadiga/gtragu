e3f9cf4ad01f668180cfce076db4d5e3f43a5f77
ravikirandevadiga <39620148+ravikirandevadiga@users.noreply.github.com>
2020-09-22 16:16:36 +0530

    added v 1
    added website

---
+++ per/js/scripts.js
@@ -0,0 +1,24 @@
+jQuery(function ($) {
+
+    'use strict';
+
+    // --------------------------------------------------------------------
+    // PreLoader
+    // --------------------------------------------------------------------
+
+    (function () {
+        $('#preloader').delay(200).fadeOut('slow');
+    }());
+
+
+}); // JQuery end
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
